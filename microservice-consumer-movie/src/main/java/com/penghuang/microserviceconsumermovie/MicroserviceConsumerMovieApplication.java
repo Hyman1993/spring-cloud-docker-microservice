f3e1767f551c5ccff442f9efd12bb1713ffd701f
Hyman1993 <1392841128@qq.com>
2022-01-19 17:12:34 +0900

    2022.01.19 Integrate Feign with Hystrix

--- microservice-consumer-movie/src/main/java/com/penghuang/microserviceconsumermovie/MicroserviceConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/penghuang/microserviceconsumermovie/MicroserviceConsumerMovieApplication.java
@@ -2,16 +2,22 @@ package com.penghuang.microserviceconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//@EnableFeignClients
+@EnableHystrix
 public class MicroserviceConsumerMovieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
 	}
 
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
--- microservice-consumer-movie/src/main/java/com/penghuang/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/penghuang/microserviceconsumermovie/controller/MovieController.java
@@ -1,10 +1,14 @@
 package com.penghuang.microserviceconsumermovie.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.penghuang.microserviceconsumermovie.dto.UserDto;
+import com.penghuang.microserviceconsumermovie.service.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +23,35 @@ public class MovieController {
     @Value("${user.userServiceUrl}")
     private String userServiceUrl;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+//    @Autowired
+//    private UserFeignClient userFeignClient;
+
     @GetMapping("/user/{id}")
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
     public UserDto findById(@PathVariable Long id) {
         return this.restTemplate.getForObject(userServiceUrl + id, UserDto.class);
+//           return  this.userFeignClient.findById(id);
+    }
+
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("" +
+                "microservice-provider-user");
+
+        MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(),
+                serviceInstance.getHost(), serviceInstance.getPort());
     }
 
 
+    public UserDto findByIdFallback(Long id, Throwable throwable) {
+        LOGGER.info("进入回退方法,异常:" + throwable);
+        UserDto user = new UserDto();
+        user.setId((long) 1);
+        user.setName("默认用户");
+        return user;
 
-
+    }
 }
